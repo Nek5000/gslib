982ed9f574dcc5e1c9ff6fb0e2bce6fb0d5cc89a
Sebastian Grimberg <sjg@amazon.com>
2024-06-17 13:52:19 -0700

    Fix misleading indentation

--- src/findpts_imp.h
+++ src/findpts_imp.h
@@ -278,7 +278,10 @@ void findptsms(        uint   *const        code_base, const unsigned       code
                  const uint   *const session_id_match, const uint                   npt,
                        struct findpts_data *const fd)
 {
-  if (fd->fevsetup==1) array_free(&fd->savpt); fd->fevsetup=0;
+  if (fd->fevsetup==1) {
+    array_free(&fd->savpt);
+    fd->fevsetup=0;
+  }
   const uint np = fd->cr.comm.np, id=fd->cr.comm.id;
   struct array hash_pt, src_pt, out_pt;
   double *distv = tmalloc(double,npt);
